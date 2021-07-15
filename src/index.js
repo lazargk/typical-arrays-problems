d9c52a9f2f9d5d0998a5cc66a892f27a6c87d356
Lazar Gumarov <54982041+lazargk@users.noreply.github.com>
2021-07-15 10:04:13 +0500

    feat: test done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (undefined == array || null == array || 0 == array.length){
+    return 0;
+  }
+  let minElem = array[0];
+  for(let i=0; i < array.length; i++){
+    if(array[i] < minElem) {
+      minElem = array[i];
+    }
+  }
+  return minElem;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (undefined == array || null == array || 0 == array.length){
+    return 0;
+  }
+  let maxElem = array[0];
+  for(let i=0; i < array.length; i++){
+    if(array[i] > maxElem) {
+      maxElem = array[i];
+    }
+  }
+  return maxElem;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (undefined == array || null == array || 0 == array.length){
+    return 0;
+  }
+  let sum = 0;
+  for(let i=0; i < array.length; i++){
+    sum = sum + array[i]
+  }
+  return sum/array.length;
 }
